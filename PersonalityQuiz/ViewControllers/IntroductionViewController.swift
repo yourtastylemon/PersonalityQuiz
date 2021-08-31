29364fb83c0b9f657112fef77239df40056af14b
Daria Kurokhtina <dashalemon96@gmail.com>
2021-08-31 21:29:32 +0300

    Initial Commit

---
+++ PersonalityQuiz/ViewControllers/IntroductionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  IntroductionViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Alexey Efimov on 26.08.2021.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+    
+    @IBAction func unwind(segue: UIStoryboardSegue) {}
+}
+
---
+++ PersonalityQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResultViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Alexey Efimov on 30.08.2021.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    // 1. Передать сюда массив с ответами
+    // 2. Определить наиболее часто встречающийся тип животного
+    // 3. Отобразить результат в соответствии с этим животным
+    // 4. Избавиться от кнопки возврата на предыдущий экран
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
